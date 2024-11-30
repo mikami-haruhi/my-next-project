2a36fb63136b373a862c98a9ee1e737568146184
mikami-haruhi <ktc24a31e0014@edu.kyoto-tech.ac.jp>
2024-11-30 16:27:39 +0900

    2024-11-30

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import styles from "./page.module.css";
 import Image from "next/image"; 
 
+
 import NewsList from "@/app/_components/NewsList";
 import ButtonLink from "@/app/_components/ButtonLink";
 import { News } from "@/app/_libs/microcms";
