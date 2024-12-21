ed39face3703631ded3fcf04d7113d8dfeb00acf
mikami-haruhi <ktc24a31e0014@edu.kyoto-tech.ac.jp>
2024-12-21 16:08:09 +0900

    サイト全体にrevalidateを設定する

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -12,7 +12,7 @@ type Props = {
         dk?: string;
     };
 }
-export const rebalidate = 60;
+
 
 export default async function Page({ params, searchParams }: Props) {
     const data =await getNewsDetail(params.slug, {
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import NewsList from "@/app/_components/NewsList";
 import ButtonLink from "@/app/_components/ButtonLink";
 
 
-
+export const revalidaate = 60;
 
 export default async function Home(){
   const data = await getNewsList({
