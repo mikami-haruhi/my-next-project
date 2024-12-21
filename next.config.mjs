d1bc29b85065008724565802c304a270e24a937d
mikami-haruhi <ktc24a31e0014@edu.kyoto-tech.ac.jp>
2024-12-21 14:15:35 +0900

    aaaaa

--- next.config.mjs
+++ next.config.mjs
@@ -11,3 +11,11 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+
+module.exports = {
+  env: {
+    MICROCMS_SERVICE_DOMAIN: process.env.MICROCMS_SERVICE_DOMAIN,
+    MICROCMS_API_KEY: process.env.MICROCMS_API_KEY,
+  },
+};
